be43673ca3fe9e7402a74e795c14b20fe3bf6720
Andy Widdowson <widdowson@brandlogic.com>
2012-01-26 11:02:01 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'bc9e4d1b5519db56e953a40ef7e6b70501f809856b456490edab2ce41971fcc572ac2a5c8f37c1dde4e93b432ca07ed74c1035d6090d4031d443b3e41cc75b49'
